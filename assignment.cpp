40ab613d07fc327ccc389ef1beb5eae3e117c919
Carson Tyler <carsontyler@mail.weber.edu>
2017-10-10 12:39:36 -0600

    Added new Binary file

--- assignment.cpp
+++ assignment.cpp
@@ -11,20 +11,24 @@ class Counter
 private:
     unsigned int count;                     // count
 public:
-    Counter():count(0){};                   // Constructor with no arguments
-    Counter(int c):count(c){};              // Constructor with argument
+    Counter():count(0){}                   // Constructor with no arguments
+    Counter(int c):count(c){}              // Constructor with argument
 
     unsigned int get_count()                // Return count
     {
         return count;
     }
-    Counter operator ++()                      // Increment prefix, 'operator' overloads the operator. We have to add the features
+    Counter operator ++()                   // Increment prefix, 'operator' overloads the operator. We have to add the features
     {
         ++count;
         return Counter(count);
         /*Counter temp;
         temp.count = count;
         return temp;*/
+    }
+    Counter operator ++ (int)               // Increment postfix
+    {
+        return Counter(count++);
     }
 };
 
@@ -33,7 +37,7 @@ public:
 // Main Program
 int main()
 {
-    Counter c1, c2, c3;
+    Counter c1, c2, c3, c4;
     cout << "c1 = " << c1.get_count() << endl;
     cout << "c2 = " << c2.get_count() << endl;
     ++c1;
@@ -42,7 +46,9 @@ int main()
     cout << "c2 = " << c2.get_count() << endl;
     c3 = ++c1;
     cout << "c3 = " << c3.get_count() << endl;
-
+    c4 = c1++;
+    cout << "c1 = " << c1.get_count() << endl;
+    cout << "c4 = " << c4.get_count() << endl;
     return 0;
 }
 // Function Definitions
---
+++ binaryOperator.cpp
@@ -0,0 +1,59 @@
+// File: binaryOperator
+// Created by Carson Tyler on 10/10/2017 for CS1410.
+// Copyright ©2017 WSU
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Constants, Structs, Classes
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    //Constructor
+    Distance() : feet(0), inches(0.0){}                         // No arguments
+    Distance(int f, float i) {feet = f; inches = i;}            // Two arguments
+
+    void getdist()                                              // Get the length for the user
+    {
+        cout << "\nEnter feet: ";
+        cin >> feet;
+        cout << "Enter inches: ";
+        cin >> inches;
+    }
+    void showdist()                                             // Display distance
+    {
+        cout << feet << "\'-" << inches << "\"" << endl;
+    }
+    Distance operator + (Distance) const;                       // Add 2 distances
+};
+//Prototypes, Return sum
+Distance Distance::operator+(Distance d2) const
+{
+    int f = feet + d2.feet;
+    float i = inches + d2.inches;
+    while (i >= 12.0)
+    {
+        i -= 12.0;
+        f++;
+    }
+    return Distance(f,i);
+}
+
+// Main Program
+int main()
+{
+    Distance d1, d3;
+    Distance d2(11, 6.25);
+    d1.getdist();
+    cout << "Distance d1 = "; d1.showdist(); cout << endl;
+    cout << "Distance d2 = "; d2.showdist(); cout << endl;
+    // Add binary operator support
+    d3 = d1 + d2;
+    cout << "Distance d3 = "; d3.showdist(); cout << endl;
+
+    return 0;
+}
+// Function Definitions
